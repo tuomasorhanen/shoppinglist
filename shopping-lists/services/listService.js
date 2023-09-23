f466376c9ee0734145dcc8a26e1ac1dd9cb8d6c9
Tuomas <tuomas.orhanen@amban.fi>
2023-09-23 15:13:27 +0300

    console log errors

--- shopping-lists/services/listService.js
+++ shopping-lists/services/listService.js
@@ -1,9 +1,18 @@
 import { executeQuery } from "../database/database.js";
 
 const getActiveShoppingLists = async () => {
-  const result = await executeQuery("SELECT * FROM shopping_lists WHERE active = true");
-  return result.rows;
+  try {
+    const result = await executeQuery("SELECT * FROM shopping_lists WHERE active = true");
+    if (!result.rows) {
+      console.log("Result rows are undefined");
+    }
+    return result.rows;
+  } catch (e) {
+    console.error("Error in getActiveShoppingLists:", e);
+    throw e;
+  }
 };
+
 
 const addNewShoppingList = async (name) => {
   await executeQuery("INSERT INTO shopping_lists (name) VALUES ($name)", { name });
