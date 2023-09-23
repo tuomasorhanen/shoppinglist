4f03bbacd38be139b1230de0a8b3fe0a1b2091e5
Tuomas <tuomas.orhanen@amban.fi>
2023-09-23 17:52:30 +0300

    push new

--- shopping-lists/services/listService.js
+++ shopping-lists/services/listService.js
@@ -1,16 +1,16 @@
-import { executeQuery } from "../database/database.js";
+import { sql } from "../database/database.js";
 
 const getActiveShoppingLists = async () => {
-  const result = await executeQuery("SELECT * FROM shopping_lists WHERE active = true");
-  return result.rows;
+  const result = await sql`SELECT * FROM shopping_lists WHERE active = true`;
+  return result;
 };
 
 const addNewShoppingList = async (name) => {
-  await executeQuery("INSERT INTO shopping_lists (name) VALUES ($name)", { name });
+  await sql`INSERT INTO shopping_lists (name) VALUES (${name})`;
 };
 
 const deactivateShoppingList = async (id) => {
-  await executeQuery("UPDATE shopping_lists SET active = false WHERE id = $id", { id });
+  await sql`UPDATE shopping_lists SET active = false WHERE id = ${id}`;
 };
 
 export { getActiveShoppingLists, addNewShoppingList, deactivateShoppingList };
