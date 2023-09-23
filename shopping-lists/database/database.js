55c7e03ff398dec5f8f4ce6bb575a80ee577c2c1
Tuomas <tuomas.orhanen@amban.fi>
2023-09-23 21:53:28 +0300

    go go go

--- shopping-lists/database/database.js
+++ shopping-lists/database/database.js
@@ -1,26 +1,14 @@
 import { Pool } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 
 const CONCURRENT_CONNECTIONS = 2;
-let connectionPool;
-
-function getPool() {
-  if (!connectionPool) {
-    if (Deno.env.get("DATABASE_URL")) {
-      connectionPool = new Pool({ connectionString: Deno.env.get("DATABASE_URL") }, CONCURRENT_CONNECTIONS);
-    } else {
-      connectionPool = new Pool({}, CONCURRENT_CONNECTIONS);
-    }
-  }
-  return connectionPool;
-}
+const connectionPool = new Pool({ connectionString: Deno.env.get("DATABASE_URL") }, CONCURRENT_CONNECTIONS);
 
 const executeQuery = async (query, params) => {
-  const pool = getPool();
   const response = {};
   let client;
 
   try {
-    client = await pool.connect();
+    client = await connectionPool.connect();
     const result = await client.queryObject(query, params);
     if (result.rows) {
       response.rows = result.rows;
