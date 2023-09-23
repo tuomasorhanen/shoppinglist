61e6b130cb296d0eec49d77a90c092a2eb6155c4
Tuomas <tuomas.orhanen@amban.fi>
2023-09-23 21:08:40 +0300

    new try

--- shopping-lists/database/database.js
+++ shopping-lists/database/database.js
@@ -1,12 +1,21 @@
 import { Pool } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 
 const CONCURRENT_CONNECTIONS = 2;
+
+const defaultConfig = {
+  user: Deno.env.get("PGUSER"),
+  password: Deno.env.get("PGPASSWORD"),
+  hostname: Deno.env.get("PGHOST"),
+  port: parseInt(Deno.env.get("PGPORT")),
+  database: Deno.env.get("PGDATABASE"),
+};
+
 let pool;
 
 if (Deno.env.get("DATABASE_URL")) {
   pool = new Pool({ connectionString: Deno.env.get("DATABASE_URL") }, CONCURRENT_CONNECTIONS);
 } else {
-  pool = new Pool({}, CONCURRENT_CONNECTIONS);
+  pool = new Pool(defaultConfig, CONCURRENT_CONNECTIONS);
 }
 
 const executeQuery = async (query, params) => {
