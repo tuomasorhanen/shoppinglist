2e7fd33874a4195efc040a2498e4d8e11e59d244
Tuomas <tuomas.orhanen@amban.fi>
2023-09-23 17:45:41 +0300

    my

--- shopping-lists/app.js
+++ shopping-lists/app.js
@@ -1,28 +1,10 @@
 import { serve } from "./deps.js";
-import { configure, renderFile } from "./deps.js";
 import { sql } from "./database/database.js";
-
-configure({
-  views: `${Deno.cwd()}/views/`,
-});
-
-const responseDetails = {
-  headers: { "Content-Type": "text/html;charset=UTF-8" },
-};
-
-const data = {
-  count: 0,
-};
 
 const handleRequest = async (request) => {
   const url = new URL(request.url);
-  if (url.pathname === "/count") {
-    data.count++;
-    return new Response(await renderFile("count.eta", data), responseDetails);
-  }
-
   if (url.pathname === "/") {
-    const rows = await sql`SELECT COUNT(*) as count FROM shopping-lists`;
+    const rows = await sql`SELECT COUNT(*) as count FROM shopping_lists`;
     let rowCount = -42;
     if (rows.length > 0) {
       rowCount = rows[0].count;
@@ -31,7 +13,7 @@ const handleRequest = async (request) => {
     return new Response(`Total rows: ${rowCount}`);
   }
 
-  return new Response("Hello you!");
+  return new Response("Not found", { status: 404 });
 };
 
-serve(handleRequest, { port: 7777 });
+await serve(handleRequest, { port: 7777 });
