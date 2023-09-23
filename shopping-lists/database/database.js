910d4b724c6ebce606e387ff9ac192208dfe80a7
Tuomas <tuomas.orhanen@amban.fi>
2023-09-23 20:56:22 +0300

    try error handling

--- shopping-lists/database/database.js
+++ shopping-lists/database/database.js
@@ -1,35 +1,24 @@
-import { Pool } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
+import { postgres } from "../deps.js";
 
-const CONCURRENT_CONNECTIONS = 2;
-let connectionPool;
-
+let sql;
 if (Deno.env.get("DATABASE_URL")) {
-  connectionPool = new Pool({ connectionString: Deno.env.get("DATABASE_URL") }, CONCURRENT_CONNECTIONS);
+  sql = postgres(Deno.env.get("DATABASE_URL"));
 } else {
-  connectionPool = new Pool({}, CONCURRENT_CONNECTIONS);
+  sql = postgres({});
 }
 
 const executeQuery = async (query, params) => {
   const response = {};
-  let client;
-
   try {
-    client = await connectionPool.connect();
-    const result = await client.queryObject(query, params);
-    if (result.rows) {
+    const result = await sql.queryObject({ text: query, args: params });
+    if (result && result.rows) {
       response.rows = result.rows;
     }
   } catch (e) {
     response.error = e;
-  } finally {
-    try {
-      await client.release();
-    } catch (e) {
-      console.log(e);
-    }
   }
 
   return response;
 };
 
-export { executeQuery };
+export { sql, executeQuery };
