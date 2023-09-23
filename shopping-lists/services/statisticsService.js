db11c5d426dc3c5d8780189170cbf98d3777f6b4
Tuomas <tuomas.orhanen@amban.fi>
2023-09-23 14:52:34 +0300

    error handling

--- shopping-lists/services/statisticsService.js
+++ shopping-lists/services/statisticsService.js
@@ -1,13 +1,28 @@
-import { executeQuery } from "../database/database.js";
-
 const getStatistics = async () => {
-  const shoppingListCount = await executeQuery("SELECT COUNT(*) as count FROM shopping_lists");
-  const shoppingListItemCount = await executeQuery("SELECT COUNT(*) as count FROM shopping_list_items");
+  let shoppingListsCount = 0;
+  let shoppingListItemsCount = 0;
+
+  try {
+    const shoppingListCountResult = await executeQuery("SELECT COUNT(*) as count FROM shopping_lists");
+    const shoppingListItemCountResult = await executeQuery("SELECT COUNT(*) as count FROM shopping_list_items");
+
+    if (shoppingListCountResult.rows) {
+      shoppingListsCount = shoppingListCountResult.rows[0]?.count || 0;
+    }
+    
+    if (shoppingListItemCountResult.rows) {
+      shoppingListItemsCount = shoppingListItemCountResult.rows[0]?.count || 0;
+    }
+  } catch (error) {
+    console.log('Database error:', error);
+    return { error: 'Database error' };
+  }
 
   return {
-    shoppingListsCount: shoppingListCount.rows[0]?.count || 0,
-    shoppingListItemsCount: shoppingListItemCount.rows[0]?.count || 0,
+    shoppingListsCount,
+    shoppingListItemsCount,
   };
 };
+
 
 export { getStatistics };
